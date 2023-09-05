e9b8c8a0c0b249783f9b4ddcf1ebaeb5f0f15386
Ahmed Zedan <zedan@gmail.com>
2023-09-05 17:46:32 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header/>
+      <Cycle/>
     </div>
   );
 }
 
-export default App;
+function Header(){
+  return(
+    <nav>
+      <h1>LOGO</h1>
+      <ul>
+        <li className="active">home</li>
+        <li>summer collection</li>
+        <li>categories</li>
+        <li>about us</li>
+      </ul>
+      <div className="buttons">
+        <button className="active">login</button>
+        <button>sign up</button>
+      </div>
+    </nav>
+  );
+}
+
+function Cycle(){
+  return(
+    <div className="container">
+      <div className="cycle">
+        <div className="top">
+            <img src="img/1.jpg" alt="style1" className="one"/>
+            <img src="img/2.jpg" alt="style2" className="two"/>
+            <img src="img/3.jpg" alt="style3" className="three"/>
+        </div>
+
+        <div className="bottom">
+            <img src="img/4.jpg" alt="style4" className="four"/>
+            <img src="img/5.jpg" alt="style5" className="five"/>
+            <img src="img/6.jpg" alt="style6" className="six"/>
+        </div>
+      </div>
+    </div>
+  );
+}
